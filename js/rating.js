e8cbca8f32add524304eef43184e9ff524114db3
Nigusu Solomon <nigusuheaven@gmail.com>
2021-07-09 18:01:40 +0900

    new js file class added

---
+++ js/rating.js
@@ -0,0 +1,16 @@
+    "use strict";
+    jQuery( document ).ready(function() {
+		var container = jQuery('.Scrappers_project_AASTU-five-star-wp-rate-action');
+		if (container.length) {
+			container.find('a').on('click', function() {
+				container.remove();
+				jQuery.post(
+					ajaxurl,
+					{
+						action: 'Scrappers_project_AASTU-five-star-wp-rate'
+					},
+					function(result) {}
+				);
+			});
+		}
+	});
