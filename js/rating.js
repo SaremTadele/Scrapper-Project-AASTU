03c4d2253424752474b278565659cadf1702c594
Restyled.io <commits@restyled.io>
2021-07-09 09:04:41 +0000

    Restyled by prettier

--- js/rating.js
+++ js/rating.js
@@ -1,12 +1,14 @@
 "use strict";
-jQuery(document).ready(function() {
-  var container = jQuery('.Scrappers_project_AASTU-five-star-wp-rate-action');
+jQuery(document).ready(function () {
+  var container = jQuery(".Scrappers_project_AASTU-five-star-wp-rate-action");
   if (container.length) {
-    container.find('a').on('click', function() {
+    container.find("a").on("click", function () {
       container.remove();
-      jQuery.post(ajaxurl,
-                  {action : 'Scrappers_project_AASTU-five-star-wp-rate'},
-                  function(result) {});
+      jQuery.post(
+        ajaxurl,
+        { action: "Scrappers_project_AASTU-five-star-wp-rate" },
+        function (result) {}
+      );
     });
   }
 });
